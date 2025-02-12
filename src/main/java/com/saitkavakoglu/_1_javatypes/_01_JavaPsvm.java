baa0f6dd17086d2563342ba841d6d1a38f8f2f59
saitama <saitama.com>
2025-02-12 22:13:07 +0300

    JavaPrimitiveTypes

--- src/main/java/com/saitkavakoglu/_1_javatypes/_01_JavaPsvm.java
+++ src/main/java/com/saitkavakoglu/_1_javatypes/_01_JavaPsvm.java
@@ -1,13 +1,11 @@
 package com.saitkavakoglu._1_javatypes;
 
-import javax.print.attribute.HashDocAttributeSet;
-
 // single comment
 /*
 multiple-
 comment
 */
-public class _01_JavaTypes {
+public class _01_JavaPsvm {
     //psvm
     public static void main(String[] args) {
         //output:sout
---
+++ src/main/java/com/saitkavakoglu/_1_javatypes/_02_JavaPrimitiveTypes.java
@@ -0,0 +1,33 @@
+package com.saitkavakoglu._1_javatypes;
+
+public class _02_JavaPrimitiveTypes
+{
+    public static void main(String[] args) {
+        //Primitive Types 8 tanedir.null değer alamazsınız.
+        //tam sayılar(4)
+        byte b1=127;
+        System.out.println("byte:"+b1);
+        short s1=32767;
+        System.out.println("short:"+s1);
+        int i1=2147483640;
+        System.out.println("int:"+i1);
+        long l1=92343534556456L;
+        System.out.println("long:"+l1);
+
+        //virgüllü(floating point)sayılar
+        float f1=3.14f;
+        System.out.println("float:"+f1);
+        double d1=3.14;
+        System.out.println("double:"+d1);
+
+        //boolean
+        boolean b3=true;
+        System.out.println("boolean:"+b3);
+       //char
+       char c1='a';
+        System.out.println("char:"+c1);
+
+
+    }
+
+}
