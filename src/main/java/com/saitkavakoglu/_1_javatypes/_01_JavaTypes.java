1d042cb6c07eca583b829c7512185472d692c662
saitama <saitama.com>
2025-02-12 20:31:20 +0300

    class, psvm, sout

--- src/main/java/com/saitkavakoglu/_1_javatypes/_01_JavaTypes.java
+++ src/main/java/com/saitkavakoglu/_1_javatypes/_01_JavaTypes.java
@@ -1,4 +1,18 @@
 package com.saitkavakoglu._1_javatypes;
 
+import javax.print.attribute.HashDocAttributeSet;
+
+// single comment
+/*
+multiple-
+comment
+*/
 public class _01_JavaTypes {
-}
+    //psvm
+    public static void main(String[] args) {
+        //output:sout
+        System.out.println(" saitama ");
+    }//end psvm
+
+
+}//end class
