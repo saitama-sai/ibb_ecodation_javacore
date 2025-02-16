537f56474ae6a283a11e681a5d9356e3847d5745
saitama <saitama.com>
2025-02-16 20:41:24 +0300

    Operatorler

---
+++ src/main/java/com/saitkavakoglu/_1_javatypes/_04_Operators.java
@@ -0,0 +1,31 @@
+package com.saitkavakoglu._1_javatypes;
+
+public class _04_Operators {
+    public static void main(String[] args) {
+        // operator
+        // +,_,*,/
+        // %
+        // ++
+        // __
+        int number1 = 23;
+        int number2 =7;
+        int addition = (number1 + number2);
+        System.out.println("Addition: " + addition);
+        int subtraction = (number1 - number2);
+        System.out.println("Subtraction: " + subtraction);
+        int multiplication = number1 * number2;
+        System.out.println("Multiplication: " + multiplication);
+        int division = number1 / number2;
+        System.out.println("Division: " + division);
+        int modulo = number1 % number2;
+        System.out.println("Modulo: " + modulo);
+
+        // Mantıksal Operators : && =)VE   || =)VEYA   ! =) değil
+        //  ++ 1 artır  -- 1 azalt
+
+
+
+
+
+    }
+}
